602d3a3c6c1d000b4737e303e2cfed4c4525c371
Maryna Pogrebniak <ms.pogrebnyak@gmail.com>
2023-06-22 16:45:57 +0200

    update project

--- src/test/java/com/phonebook/tests/RemoveContactTests.java
+++ src/test/java/com/phonebook/tests/RemoveContactTests.java
@@ -1,11 +1,10 @@
 package com.phonebook.tests;
 
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class RemoveContactTest extends TestBase {
+public class RemoveContactTests extends TestBase {
 
     @BeforeMethod
     public void ensurePrecondition(){
